186bf458373bde3c2ec58b602dfead702daa4b4e
speckledegg77 <mark@scraptastic.co.uk>
2026-03-06 11:10:22 +0000

    Create QRTile.tsx

---
+++ components/ui/QRTile.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { QRCodeSVG } from "qrcode.react"
+
+type Props = {
+  value: string
+  size?: number
+}
+
+export default function QRTile({ value, size = 112 }: Props) {
+  return (
+    <div className="rounded-xl border border-[var(--border)] bg-white p-2">
+      <QRCodeSVG
+        value={value}
+        size={size}
+        includeMargin={true}
+        level="M"
+        bgColor="#ffffff"
+        fgColor="#000000"
+      />
+    </div>
+  )
+}
